84320e6fc4e772be7dbf6ce431386d84ef54fdfe
Ultrman82 <exelcior99@gmail.com>
2019-10-26 20:19:23 -0700

    Enemy init

--- header/Enemy.hpp
+++ header/Enemy.hpp
@@ -1,18 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Enemy.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chjeong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/26 19:56:36 by chjeong           #+#    #+#             */
+/*   Updated: 2019/10/26 19:56:39 by chjeong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ENEMY_HPP
 # define ENEMY_HPP
 
-class        Enemy {
+# include <iostream>
+# include <string>
 
-    public:
-        Enemy();
-        ~Enemy( void );
-        Enemy( Enemy const & enemy );
-        Enemy &operator=(Enemy const & enemy);
+class Enemy
+{
+public:
+    Enemy(int);    
+    Enemy(void);
+    ~Enemy(void);
+    Enemy( Enemy const & enemy );
+    Enemy &operator=(Enemy const & enemy);    
+    void setName(std::string str);
+    void announce(void);
+    void move(int x, int y);
+    int x;
+    int y;       
 
-    private:
-        int	x_center;
-        int y_center;
-
+private:
+    std::string _type;
+    std::string _name;
+    int         _index;    
 };
 
 #endif
